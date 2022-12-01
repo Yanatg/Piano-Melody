ac8cb0b606a1bb2885e9ef2559764874f534102a
Yanatchara JERAJA <yanatchara.j@ku.th>
2022-12-01 20:00:29 +0700

    minor edit

--- note.py
+++ note.py
@@ -18,7 +18,6 @@ class Note:
 
     def play_music(self, name):
         """ play music that the user picked """
-        # for data in self.dict_note[0]:
         for music, notes in self.all_note[0].items():
             if music == name:
                 current_m = {name: notes}
